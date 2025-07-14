4c073c890340a9ad443e6c06e4325f17cd135d14
김다현 <dahyun2004@inu.ac.kr>
2025-07-15 03:10:21 +0900

    Design: 공통 컴포넌트 개발발

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import Container from '../components/Container'
+//import Bigbtn from '../components/buttons/Bigbtn'
+import Smallbtn from '../components/buttons/Smallbtn'
+
+function Login() {
+    return (
+        <>
+            <Smallbtn>다음단계로</Smallbtn>
+            <Container> 
+                <h1>Login</h1>      
+            </Container>
+        </>
+    )
+}
+
+export default Login;
