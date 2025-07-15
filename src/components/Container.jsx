8223597b45958d538cfa8724f92e008b641cccef
choi-il-woo <chldlfdn0440@naver.com>
2025-07-15 17:41:05 +0900

    hotfix: 컨테이너 오류 수정

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,29 @@
-function Container() {
-  return (
-    <div
-      style={{
-        position: 'fixed',
-        top: 'calc(50% + 40px)',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: '1220px',
-        height: '693px',
-        backgroundColor: 'white',
-        borderRadius: '20px',
-        boxShadow: '0 4px 16px rgba(0,0,0,0.08)',
-      }}
-    ></div>
-  )
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const ContainerWrapper = styled.div`
+  width: 80vw;
+  height: 70vh;
+  padding: 30px 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  flex-shrink: 0;
+  border-radius: 30px;
+  background: #fff;
+  margin: 9vh auto;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+  box-sizing: border-box;
+`
+
+function Container({ children, ...props }) {
+  return <ContainerWrapper {...props}>{children}</ContainerWrapper>
+}
+
+Container.propTypes = {
+  children: PropTypes.node,
 }
 
 export default Container
