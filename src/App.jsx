1aa1eeebeb373cb504390a5fdf767b57a47a2c79
yoo_ming <ohyoom9244@tukorea.ac.kr>
2025-07-15 21:04:39 +0900

    Design: 업로드 페이지 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import GlobalStyle from './styles/GlobalStyle'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Onboarding from './pages/Onboarding'
 import Login from './pages/Login'
+import Upload from './pages/Upload'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/Onboarding" element={<Onboarding />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Upload" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   )
