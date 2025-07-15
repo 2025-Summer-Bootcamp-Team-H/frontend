2c0d47ce5a1198cb81cde60ea7161346b801f9f6
yoo_ming <ohyoom9244@tukorea.ac.kr>
2025-07-15 16:44:09 +0900

    hotfix: 컨테이너 위치 조정

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,19 @@
 function Container() {
-    return (
-        <div style={{
-            position: 'fixed',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -40%)',
-            width: '1220px',
-            height: '693px',
-            backgroundColor: 'white',
-            borderRadius: '20px',
-            boxShadow: '0 4px 16px rgba(0,0,0,0.08)'
-        }}>
-            
-        </div>
-    );
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 'calc(50% + 40px)',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '1220px',
+        height: '693px',
+        backgroundColor: 'white',
+        borderRadius: '20px',
+        boxShadow: '0 4px 16px rgba(0,0,0,0.08)',
+      }}
+    ></div>
+  )
 }
 
-export default Container;
+export default Container
